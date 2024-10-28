c29104ceffd43eddeb259c2d731e774b074fbb9c
Asmaa Marzouk <fsd531758@gmail.com>
2024-10-28 13:24:21 +0300

    Third Commit

--- js/custom.js
+++ js/custom.js
@@ -304,7 +304,7 @@
       });
 
 
-      if(document.getElementById("section-portfolio")){
+      if(document.getElementById("section-projects")){
             var $grid = $(".grid").isotope({
               itemSelector: ".all",
               percentPosition: true,
